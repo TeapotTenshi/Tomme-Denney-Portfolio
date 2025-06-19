d3caa44827045543bc5cc26b8bf887fd680afd88
Tomme <denneytomme@gmail.com>
2025-06-19 18:47:11 -0400

    remove

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".project-title").forEach(button => {
-    button.addEventListener("click", () => {
-        const card = button.closest(".project-card");
-        const expanded = card.classList.toggle("expanded");
-        button.setAttribute("aria-expanded", expanded);
-    });
-});
